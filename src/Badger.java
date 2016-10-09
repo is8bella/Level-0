4eac823a0ed31b30a3364c18becd6ae08eb26874
League <jiwenzou@gmail.com>
2016-10-08 17:30:00 -0700

    6

---
+++ src/Badger.java
@@ -0,0 +1,19 @@
+
+public class Badger {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 2; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.println("Badger");
+			}
+			for (int j = 0; j < 2; j++) {
+				System.out.println("Mushroom");
+			}
+			for (int j = 0; j < 10; j++) {
+				System.out.println("Badger");
+			}
+			for (int j = 0; j < 2; j++) {
+				System.out.println("Snake");
+			}
+		}
+	}
+}
---
+++ src/Beer.java
@@ -0,0 +1,11 @@
+
+public class Beer {
+	public static void main(String[] args) {
+		for (int i = 99; i >= 1; i--) {
+			System.out.println(i + " bottles of beer on the wall");
+			System.out.println("Take one down and pass it around, " + --i + " bottles of beer on the wall");
+			}
+		}
+	}
+
+
